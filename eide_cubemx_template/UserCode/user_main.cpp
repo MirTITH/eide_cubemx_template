c876c9ff08e467b0d936fd7e3478e4af67639e31
TITH <1023515576@qq.com>
2023-07-09 18:09:31 +0800

    添加了一些文件，使得文件夹完整

---
+++ eide_cubemx_template/UserCode/user_main.cpp
@@ -0,0 +1,16 @@
+#include "user_main.h"
+#include "main.h"
+#include "FreeRTOS.h"
+#include "task.h"
+
+void StartDefaultTask(void const *argument)
+{
+    (void)argument;
+
+    while (true) {
+        HAL_GPIO_TogglePin(Led_GPIO_Port, Led_Pin);
+        vTaskDelay(200);
+    }
+
+    vTaskDelete(nullptr); // 删除当前线程
+}
